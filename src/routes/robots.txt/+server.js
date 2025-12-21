2dd92bdca899426126c64b1420da7d04972f6653
Evan Alvarez <lwq7e50ygv8f1w@tuta.com>
2025-12-21 14:08:57 -0600

    disallow ai bots in robots.txt

--- src/routes/robots.txt/+server.js
+++ src/routes/robots.txt/+server.js
@@ -1,6 +1,47 @@
+export const prerender = true;
+
 export async function GET() {
   const robots = `User-agent: *
 Allow: /
+
+User-agent: GPTBot
+Disallow: /
+
+User-agent: ChatGPT-User
+Disallow: /
+
+User-agent: CCBot
+Disallow: /
+
+User-agent: Google-Extended
+Disallow: /
+
+User-agent: anthropic-ai
+Disallow: /
+
+User-agent: Claude-Web
+Disallow: /
+
+User-agent: ClaudeBot
+Disallow: /
+
+User-agent: Bytespider
+Disallow: /
+
+User-agent: Diffbot
+Disallow: /
+
+User-agent: FacebookBot
+Disallow: /
+
+User-agent: PerplexityBot
+Disallow: /
+
+User-agent: Omgilibot
+Disallow: /
+
+User-agent: YouBot
+Disallow: /
 
 Sitemap: https://evanalvarez.dev/sitemap.xml`;
 
--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,3 +1,5 @@
+export const prerender = true;
+
 export async function GET() {
   const pages = [
     '',
